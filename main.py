5322c09244dbe7db08449750c8695b338521e2e6
Denis S <denis1123si@gmail.com>
2018-04-30 15:31:15 +0300

    Let's get it started

---
+++ main.py
@@ -0,0 +1,72 @@
+import pygame as pg
+from constants import *
+import painting as pnt
+import clouds as cl
+import lives as liv
+
+pg.init()
+win = pg.display.set_mode((win_w, win_h))  # Создание самого экрана для отрисовки
+pg.display.set_caption("TELERUN")  # Название, которое будет в шапке окна
+clock = pg.time.Clock()  # Штуковина для отсчета всяких времён
+
+# Подгружаем всякие картинки для отрисовки -----------------------------------------------------------------------------
+font_huge = pg.font.Font('cornerstone.ttf', int(72 * scaling))
+font_normal = pg.font.Font('cornerstone.ttf', int(48 * scaling))
+font_small = pg.font.Font('cornerstone.ttf', int(36 * scaling))
+
+logo = pg.image.load("logo_tr.png").convert_alpha()  # Подгружаем картинки и изменяем их размер для отрисовки
+logo = pg.transform.smoothscale(logo, (int(258 * scaling), int(192 * scaling)))
+plane = pg.image.load("plane.png").convert_alpha()
+plane = pg.transform.smoothscale(plane, (int(pl_w * scaling), int(pl_h * scaling)))
+plane_dmg = pg.image.load("plane_dmg.png").convert_alpha()
+plane_dmg = pg.transform.smoothscale(plane_dmg, (int(pl_w * scaling), int(pl_h * scaling)))
+
+cloud_0 = pg.image.load("cloud0.png").convert_alpha()  # Подгружаем картинки и изменяем их размер для отрисовки
+cloud_1 = pg.image.load("cloud1.png").convert_alpha()
+cloud_2 = pg.image.load("cloud2.png").convert_alpha()
+cloud_3 = pg.image.load("cloud3.png").convert_alpha()
+cloud0 = pg.transform.smoothscale(cloud_0, (cld_w, cld_h))
+cloud1 = pg.transform.smoothscale(cloud_1, (cld_w, cld_h))
+cloud2 = pg.transform.smoothscale(cloud_2, (cld_w, cld_h))
+cloud3 = pg.transform.smoothscale(cloud_3, (cld_w, cld_h))
+# ----------------------------------------------------------------------------------------------------------------------
+
+clouds_img = [cloud0, cloud1, cloud2, cloud3]  # Массив всех возможных форм облачков
+clouds = []  # Массив облачков, которые на экране уже бегут
+
+pl_spdx = spd  # Текущие скорости самолётика по осям
+pl_spdy = 0
+
+pl_x = midle_x  # Текущие координаты самолётика
+pl_y = midle_y
+
+vulnerable = True  # Флаг, показывающий является ли цель уязвимой в данный момент
+
+game_time = 0  # Счётчик текущего времени игры
+best_time = 0  # Тут храниться лучшее время за сессию
+
+# Всякие флаги ---------------------------------------------------------------------------------------------------------
+crashed = False  # Флаг для проверки закрывания программы
+menu = True  # Флаг, показывающий, что игрк находится (не находится) в меню
+game_over = False  # Флаг, показывающий, что игрк играет (не играет)
+game = False  # Флаг, показывающий, что игрк видит (не видит) окно GAME OVER
+# ----------------------------------------------------------------------------------------------------------------------
+
+
+while not crashed:
+    """Основной цикл, внутри которого должна быть написана обработка клавиш и порядок отрисовки для каждого из окон:
+    'MENU'
+    'GAME'
+    'GAME OVER' 
+    Также именно тут должен быть реализован счетчик времени и сохранение лучшего результата!"""
+    win.fill((255, 255, 255))
+
+    pg.time.delay(delay)
+    for event in pg.event.get():  # Проверка на выход из игры
+        if event.type == pg.QUIT:
+            crashed = True
+
+    pg.display.flip()  # Перерисовка всего экрана
+
+pg.quit()  # Завершение программы
+quit()
---
+++ painting.py
@@ -0,0 +1,94 @@
+from constants import *
+
+
+def draw_menu(win, font_small, font_normal, font_huge, logo, best_time):
+    """Процедура, при вызове которой отрисовывается экран 'MENU'
+    Аргументы:
+    win - окно отрисовки
+    font_small, font_normal, font_hugу - соотв. шрифты из main
+    logo - файл logo.png
+    best_time - лучшее время за сессию"""
+    telerun_txt = font_huge.render('TELERUN', True, (0, 137, 204))
+    win.blit(telerun_txt, [0.46 * win_w, 0.3 * win_h])
+
+    press_txt = font_normal.render('PRESS ENTER TO START...', True, (0, 137, 204))
+    win.blit(press_txt, [0.13 * win_w, 0.61 * win_h])
+
+    bestt_txt = font_small.render('BEST TIME: ' + str(round(best_time, 1)) + 'S', True, (0, 137, 204))
+    win.blit(bestt_txt, [10 * scaling, 0.93 * win_h])
+
+    win.blit(logo, (int(145 * scaling), int(108 * scaling)))
+
+
+def draw_go(win, font_small, font_normal, font_huge, game_time, best_time):
+    """Процедура, при вызове которой отрисовывается экран 'GAME OVER'
+    Аргументы:
+    win - окно отрисовки
+    font_small, font_normal, font_hugу - соотв. шрифты из main
+    game_time - время завершенной игры
+    best_time - лучшее время за сессию"""
+    go_txt = font_huge.render('GAME OVER', True, (0, 137, 204))
+    go_rec = go_txt.get_rect()
+    go_rec.center = (win_w / 2, win_h / 3)
+    win.blit(go_txt, go_rec)
+
+    yres_txt = font_normal.render('YOUR RESULT: ' + str(round(game_time, 1)) + 'S', True, (0, 137, 204))
+    yres_rec = yres_txt.get_rect()
+    yres_rec.center = (win_w / 2, win_h / 3 + 100 * scaling)
+    bestres_txt = font_normal.render('BEST RESULT: ' + str(round(best_time, 1)) + 'S', True, (0, 137, 204))
+    bestres_rec = bestres_txt.get_rect()
+    bestres_rec.center = (win_w / 2, win_h / 3 + 150 * scaling)
+    win.blit(yres_txt, yres_rec)
+    win.blit(bestres_txt, bestres_rec)
+
+    rest_txt = font_small.render('RESTART: ENTER', True, (0, 137, 204))
+    rest_rec = rest_txt.get_rect()
+    rest_rec.center = (win_w / 2, 2 * win_h / 3 + 50 * scaling)
+    back_txt = font_small.render('MAIN MENU: BACKSPACE', True, (0, 137, 204))
+    back_rec = back_txt.get_rect()
+    back_rec.center = (win_w / 2, 2 * win_h / 3 + 90 * scaling)
+    win.blit(rest_txt, rest_rec)
+    win.blit(back_txt, back_rec)
+
+
+def draw_plane(win, x, y, plane, plane_dmg, vulnerable):
+    """Процедура, при вызове которой отрисовывается самолётик
+    Аргументы:
+    win - окно отрисовки
+    x - координата самолётика по оси Х (верхний левый угол)
+    y - координата самолётика по оси Y (верхний левый угол)
+    plane - файл plane.png (обычный самолётик)
+    plane_png - файл plane_dmg.png (самолётик после ранения)
+    vulnerable - флаг, говорящий о том уязвим самолётик или нет"""
+    # Многоугольник, задающий самолётик. Будет нужен в
+    # pg.draw.polygon(win, (0, 0, 0),
+    #                 [(x + 0.21 * pl_w, y + 0.32 * pl_h), (x + 0.19 * pl_w, y), (x + pl_w, y + 0.32 * pl_h),
+    #                  (x + 0.21 * pl_w, y + pl_h), (x + 0.21 * pl_w, y + 0.75 * pl_h), (x, y + 0.85 * pl_h)], 2)
+    if vulnerable:  # Рисуем самолётик
+        win.blit(plane, (x, y))
+    else:
+        win.blit(plane_dmg, (x, y))
+
+
+def lives_counter(win, font, lives):
+    """Процедура, при вызове которой отрисовывается текущее количество жизней
+        Аргументы:
+        win - окно отрисовки
+        font - шрифт
+        lives - переменная, отвечающая за количество жизней"""
+    lives_txt = font.render(lives * "*", True, (0, 137, 204))
+    # yres_rec = yres_txt.get_rect()
+    # yres_rec.center = (win_w / 2, win_h / 3 + 100 * scaling)
+    win.blit(lives_txt, (10 * scaling, 10 * scaling))
+
+
+def print_time(win, font, time):
+    """Процедура, при вызове которой отрисовывается время текущей игры
+            Аргументы:
+            win - окно отрисовки
+            font - шрифт
+            time - переменная, отвечающая за отсчет времени"""
+    time_txt = font.render(str(round(time, 1)) + " S", True, (0, 137, 204))
+    time_rec = time_txt.get_rect()
+    time_rec.center = (win_w / 2, 27 * scaling)
+    win.blit(time_txt, time_rec)
