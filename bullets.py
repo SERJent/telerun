82b18bd849eb588ec7f072d34281e173a73e8da2
gladik00 <gladkovruslan2000@mail.ru>
2018-05-27 17:00:58 +0300

    Change constants

--- bullets.py
+++ bullets.py
@@ -32,10 +32,10 @@ def bullet_generator(win, x, y, bullet_picture):
             shot.x = shot.x + shot.v_x
             shot.y = shot.y + shot.v_y
 
-    if speed_counter < 3000:
-        bullet_speed = bullet_speed_init + bullet_speed_init * (speed_counter // 500)
+    if speed_counter < 5000:
+        bullet_speed = bullet_speed_init + bullet_speed_add * (speed_counter // 500)
     else:
-        bullet_speed = bullet_speed_init * 7
+        bullet_speed = bullet_speed_init + bullet_speed_add * 11
 
     if len(bullet_array) == 0:
         new_born_x = random.randrange(0, win_w, 5)
@@ -44,7 +44,7 @@ def bullet_generator(win, x, y, bullet_picture):
         new_born_v_y = bullet_speed * (y - new_born_y) / ((x - new_born_x) ** 2 + (y - new_born_y) ** 2) ** 0.5
         bullet_array.append(bullet(new_born_x, new_born_y, new_born_v_x, new_born_v_y, bull_w / 2, bullet_picture))
     else:
-        if bullet_array[-1].y <= win_h - 3 * bull_w:
+        if bullet_array[-1].y <= win_h - 3.5 * bull_w:
             new_born_x = random.randrange(0, win_w, 5)
             new_born_y = win_h + bull_w / 2
             new_born_v_x = bullet_speed * (x - new_born_x) / ((x - new_born_x) ** 2 + (y - new_born_y) ** 2) ** 0.5
